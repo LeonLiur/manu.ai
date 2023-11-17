27647454f19e14b16dbcac747028692a09b0032e
Léo Liu <leoliuhy@gmail.com>
2023-11-16 22:02:30 -0500

    Image works for manuals that do have image

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import chromadb
 import PyPDF2 as pypdf
-import pdfminer
 from tqdm import tqdm
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
@@ -13,7 +12,8 @@ from langchain.embeddings import OpenAIEmbeddings
 import os
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
-from pdfminer.high_level import extract_pages
+import pdfminer
+from pdfminer.high_level import extract_text, extract_pages
 from pdfminer.image import ImageWriter
 
 
@@ -70,14 +70,10 @@ async def upload_item(
     chunk_size: int = 300,
     overlap: float = 0,
 ):
-
-    pdf = pypdf.PdfReader(file.file)
-
-    document_text = get_text_from_pdf(pdf, manual_name)
-    get_images_from_pdf(pdf, manual_name)
+    text = handle_pdf(file.file, manual_name, manual_id)
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_size * overlap, length_function=len, add_start_index=True)
-    chunks = text_splitter.create_documents([document_text])
+    chunks = text_splitter.create_documents([text])
 
     collection = chroma_client.create_collection(name=manual_id)
     collection.add(
@@ -89,37 +85,26 @@ async def upload_item(
     return {"status": 200}
 
 
-def get_text_from_pdf(pdf_in, manual_name):
-    print("[*] parsing pdf for text:")
-    
-    file_path = f'./storage/{manual_name}/text.txt'
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-    document_text = ""
-    with open(file_path, "w") as f:
-        for page in tqdm(pdf_in.pages):
-            document_text += page.extract_text()
-        f.write(document_text)
+def handle_pdf(pdf_in, manual_name, manual_id):
+    text = extract_text(pdf_in)
+    os.makedirs(f"./storage/{manual_name}", exist_ok=True)
+    with open(f"./storage/{manual_name}/text.txt", "w") as f:
+        f.write(text)
         
-    return document_text
-
-def get_images_from_pdf(pdf_in, manual_name):
-    print("[*] parsing pdf for images:")
+    for page in tqdm(extract_pages(pdf_in)):
+        save_images_from_page(page, manual_name, manual_id)
+        
+    return text
+        
     
-    file_path = f'./storage/{manual_name}/images/'
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    image_id = 0
-    for page in tqdm(pdf_in.pages):
-        try:
-            for image in page.images:
-                with open(f'./storage/{manual_name}/images/{image_id}_{image.name}', "wb") as f:
-                    f.write(image.data)
-                    image_id += 1
-        except Exception as e:
-            print(f"[-] {e} occured")
-            continue
-    
+def save_images_from_page(page: pdfminer.layout.LTPage, manual_name, manual_id):
+    images = list(filter(bool, map(get_image, page)))
+    iw = ImageWriter(f'./storage/{manual_name}/images/')
+    for image in images:
+        iw.export_image(image)
+        
+
 def get_image(layout_object):
     if isinstance(layout_object, pdfminer.layout.LTImage):
         return layout_object
@@ -128,10 +113,4 @@ def get_image(layout_object):
             return get_image(child)
     else:
         return None
-
-
-def save_images_from_page(page: pdfminer.layout.LTPage):
-    images = list(filter(bool, map(get_image, page)))
-    iw = ImageWriter('output_dir')
-    for image in images:
-        iw.export_image(image)
+        
