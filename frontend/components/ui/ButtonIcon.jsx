82d4f1709b4650e9bea70a76adf2a98592fae073
“23jmo” <2023johnathanmo@gmail.com>
2023-12-04 21:27:54 -0500

    chat change to front end

--- frontend/components/ui/ButtonIcon.jsx
+++ frontend/components/ui/ButtonIcon.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function ButtonIcon(){
     return (
-        <Button className= "bg-slate-600 hover: bg-slate-400" variant="default" size="icon">
+        <Button className= "hover: bg-slate-500" variant="default" size="icon">
           <ChevronRightIcon className="h-4 w-4" />
         </Button>
     )
