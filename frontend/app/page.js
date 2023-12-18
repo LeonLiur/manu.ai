2f4bd1a9fbb1349684258d34cbf448e42526231f
Léo Liu <leoliuhy@gmail.com>
2023-12-18 14:23:15 -0500

    frontend

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,6 @@
 "use client"
 
 // pages/index.js
-import InputBox from "../components/ui/InputBox";
-import ButtonIcon from "@/components/ui/ButtonIcon";
-import SearchableDropdown from "@/components/ui/SeachableDropdown";
-import { Button } from "@/components/ui/button";
-import ChatBot from "@/components/ui/chatBot";
-import { StatDownArrow } from "@chakra-ui/react";
-import { ArrowDownNarrowWideIcon, ArrowDownRightSquare } from "lucide-react";
-import { useState } from "react";
-import HeroSection from "@/components/ui/hero-section";
 import Content from "@/components/ui/content";
 
 const Home = () => {
