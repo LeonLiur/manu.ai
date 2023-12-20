48b63ca11321e7d2694807ec2a5dec3f459f4821
“23jmo” <2023johnathanmo@gmail.com>
2023-12-20 12:32:57 -0800

    ui fixes

--- frontend/app/upload_pdf/page.js
+++ frontend/app/upload_pdf/page.js
@@ -53,7 +53,7 @@ export default function Upload() {
                 <a href="" className="text-3xl font-bold">Manu.ai</a>
             </div>
             <div className="flex mt-10 w-[700] min-w-[400] justify-center items-center">
-                <div className="flex flex-col p-10 border-2 border-gray-600 rounded-md shadow-md shadow-slate-900 gap-4">
+                <div className="flex flex-col p-10 border-2  border-gray-400 dark:border-gray-600 rounded-md shadow-md shadow-gray-300 dark:shadow-slate-900 gap-4">
                     
                     <div className="flex flex-col justify-center">
                         <p className="self-center h-12 w-12 text-3xl">📋</p>
@@ -62,19 +62,19 @@ export default function Upload() {
                     <input id="file" type="file" />
                     <div>
                         <p className="font-semibold mb-2">Manual Name</p>
-                        <input id="manualname" className="w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-gray-100 font-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600"
+                        <input id="manualname" className="w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-slate-900 font-sm shadow-sm ring-1 ring-inset dark:text-white  ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600"
                         placeholder='Ex: Samsung Spin Cycle 3000'
                         onChange={(e) => { setproductName(e.target.value) }} />
                     </div>
                     <div>
                         <p className="font-semibold mb-2">Company Name</p>
-                        <input id="companyname" className="w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-gray-100 font-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600"
+                        <input id="companyname" className="w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-slate-900 dart:text-white font-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600"
                         placeholder='Ex: Samsung'
                         onChange={(e) => { setCompanyName(e.target.value.toLowerCase()) }} />
                     </div>
                     <div>
                         <p className="font-semibold mb-2">Product Type</p>
-                        <select id="select" className="w-full rounded-md border-0 bg-white/5 pl-2 pr-3 py-2 text-gray-100 font-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600 "
+                        <select id="select" className="w-full rounded-md border-0 bg-white/5 pl-2 pr-3 py-2 text-slate-900 dark:text-white font-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 hover:ring-gray-600 "
                         onChange={(e) => { setProductType(e.target.value) }} defaultValue={""}>
                             <option className="text-gray-300" value="" disabled>Select your option</option>
                             <option value="dishwasher">Dishwasher</option>
@@ -114,7 +114,7 @@ export default function Upload() {
                         :
                         <div className="flex items-center gap-4">
                             <div id="circle-indicator" className="w-4 h-4 bg-red-500 rounded-full"></div>
-                            <p className="text-slate-300">NOT UPLOADED</p>
+                            <p className="text-slate-900 dark:text-white">NOT UPLOADED</p>
                             <div className= "flex flex-grow"></div>
                             
                         </div>
